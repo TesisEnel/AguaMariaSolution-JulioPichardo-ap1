00547878fcc7c086eba102b3ac6976f1b03d1965
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-05 18:19:14 -0400

    Creacion del contexto Version#1

---
+++ Server/DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AguaMariaSolution.Shared.Models;
+
+namespace AguaMariaSolution.Server.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Parametros> Parametros { get; set; }
+        public DbSet<Empleados> Empleados { get; set; }
+        public DbSet<ControlCalidadProductoTerminado> ControlCalidadProductoTerminado { get; set; }
+        public DbSet<ProductoTerminadosDetalle> ProductoTerminadosDetalle { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Parametros>().HasData(new List<Parametros>()
+        {
+            new Parametros(){ParametroId=1, Descripción="Cloruro", Mínimo = 0, Máximo = 1,},
+            
+        });
+        }
+    }
+}
+}
+
