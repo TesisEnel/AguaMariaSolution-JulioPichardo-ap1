ffd5de77122c21a728cb3c04188a77ba8ded8de4
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-05 18:20:20 -0400

    Cambiando Nombre a llave primaria de ControlCalidadProductoTerminado, Agregando Modelo Empleados y productoTerminadosDetalles

--- Shared/Models/ControlCalidadProductoTerminado.cs
+++ Shared/Models/ControlCalidadProductoTerminado.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace AguaMariaSolution.Shared.Models
     public class ControlCalidadProductoTerminado
     {
         [Key]
-        public int Id { get; set; }
+        public int ProductoTerminadoId { get; set; }
         [Required]
         public DateTime Fecha { get; set; } = DateTime.Now;
 
---
+++ Shared/Models/Empleados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AguaMariaSolution.Shared.Models
+{
+    public class Empleados
+    {
+        [Key]
+        public int EmpleadoId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no acepta digitos")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no acepta digitos")]
+        public string? Apellido { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Dirección { get; set; }
+
+        [ForeignKey("EmpleadoId")]
+        public ICollection<ControlCalidadProductoTerminado> ControlCalidadProductoTerminado { get; set; } = new List<ControlCalidadProductoTerminado>();
+    }
+}
