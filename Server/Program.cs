73399b0154d28e982492536dfabf95ad3393b1df
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-05 20:59:01 -0400

    Agregando el ConStr, e inyectandolo junto con el contexto en el program

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
+using AguaMariaSolution.Server.DAL;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContextFactory<Contexto>(op => op.UseSqlite(ConStr));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
