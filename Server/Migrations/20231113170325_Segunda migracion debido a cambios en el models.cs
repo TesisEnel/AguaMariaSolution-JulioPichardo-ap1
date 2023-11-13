ab282ff1f46475c6249891c9fd0618048ca2b37a
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-13 13:06:20 -0400

    Debido a cambios en el Models, Nueva Migracion y BD

---
+++ Server/Migrations/20231113170325_Segunda migracion debido a cambios en el models.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AguaMariaSolution.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Segundamigraciondebidoacambiosenelmodels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TandaId",
+                table: "ControlCalidadProductoTerminado");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TandaId",
+                table: "ControlCalidadProductoTerminado",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
