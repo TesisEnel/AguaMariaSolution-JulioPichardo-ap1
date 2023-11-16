87ace62b1828bc3a7915827a246aad1a69586b03
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-16 18:19:56 -0400

    Nueva Migracion y BD debido a cambios en el contexto

---
+++ Server/Migrations/20231116214814_Nueva migracion debido a cambios en el contexto.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AguaMariaSolution.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nuevamigraciondebidoacambiosenelcontexto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 12,
+                column: "Mínimo",
+                value: 1f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 14,
+                column: "Mínimo",
+                value: 1f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 12,
+                column: "Mínimo",
+                value: 0f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 14,
+                column: "Mínimo",
+                value: 0f);
+        }
+    }
+}
---
+++ Server/Migrations/20231116221745_Nueva migracion debido a cambios en el contexto#2.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AguaMariaSolution.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nuevamigraciondebidoacambiosenelcontexto2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 2,
+                column: "Mínimo",
+                value: 0f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 4,
+                columns: new[] { "Máximo", "Mínimo" },
+                values: new object[] { 700f, 0f });
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 9,
+                column: "Mínimo",
+                value: 0f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 10,
+                column: "Mínimo",
+                value: 0f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 11,
+                column: "Mínimo",
+                value: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 2,
+                column: "Mínimo",
+                value: -3.4028235E+38f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 4,
+                columns: new[] { "Máximo", "Mínimo" },
+                values: new object[] { 3.4028235E+38f, -3.4028235E+38f });
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 9,
+                column: "Mínimo",
+                value: -3.4028235E+38f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 10,
+                column: "Mínimo",
+                value: -3.4028235E+38f);
+
+            migrationBuilder.UpdateData(
+                table: "Parametros",
+                keyColumn: "ParametroId",
+                keyValue: 11,
+                column: "Mínimo",
+                value: -3.4028235E+38f);
+        }
+    }
+}
