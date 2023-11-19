e8272c650bf05353e0f57a43b2650de969b95697
A-EHJ <abrahamelhage2003@gmail.com>
2023-11-19 19:09:08 -0400

    Mejorando la eficiencia de la funcion Login, y haciendo que notifique al usuario que tiene erroneo. Tambien eliminando el rol cliente del login.

--- Server/Controllers/UsuarioController.cs
+++ Server/Controllers/UsuarioController.cs
@@ -21,30 +21,28 @@ namespace AguaMariaSolution.Server.Controllers
         {
             SesionAMS sesionAMS = new SesionAMS();
 
-            var empleadoEncontrado = await _context.Empleados.FirstOrDefaultAsync(e => e.Email == login.Correo && e.Clave == login.Clave);
-            var empleadoEncontradoNot = await _context.Empleados.FirstOrDefaultAsync(e => e.Email == login.Correo && e.Clave != login.Clave);
-
             if (login.Correo == "julio@gmail.com" && login.Clave == "admin" || login.Correo == "abraham@gmail.com" && login.Clave == "admin")
             {
                 sesionAMS.Nombre = "admin";
                 sesionAMS.Correo = login.Correo;
                 sesionAMS.Rol = "Administrador";
             }
-            else if (empleadoEncontrado != null)
+
+            var empleadoEncontrado = await _context.Empleados.FirstOrDefaultAsync(e => e.Email == login.Correo);
+
+            if (empleadoEncontrado != null && empleadoEncontrado.Clave==login.Clave)
             {
                 sesionAMS.Nombre = empleadoEncontrado.Nombre;
                 sesionAMS.Correo = login.Correo;
                 sesionAMS.Rol = "Empleado";
             }
-            else if (empleadoEncontradoNot != null)
+            else if (empleadoEncontrado == null)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "Usuario no encontrado");
+            }
+            else if (login.Clave != empleadoEncontrado.Clave)
             {
                 return StatusCode(StatusCodes.Status401Unauthorized, "Contraseña incorrecta");
-            }
-            else
-            {
-                sesionAMS.Nombre = "cliente";
-                sesionAMS.Correo = login.Correo;
-                sesionAMS.Rol = "Cliente";
             }
 
             return StatusCode(StatusCodes.Status200OK, sesionAMS);
