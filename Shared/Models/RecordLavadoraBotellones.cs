5689ae6c30b9691cb2f361b07f8fdd9a36d9ef5c
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-11-23 14:11:18 -0400

    Nuevo Modelo Details: Creando el modelo para la tabla de record de lavadora de botellones

---
+++ Shared/Models/RecordLavadoraBotellones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AguaMariaSolution.Shared.Models
+{
+    public class RecordLavadoraBotellones
+    {
+        [Key]
+        public int RecordId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public float Temperatura { get; set; }
+        public float Detergente { get; set; }
+        public string? AlcalinidadEnjuague { get; set; }
+        public string? DesinfectanteConcentración { get; set; }
+        public int ClienteId { get; set; }
+        public string? AcciónTomada { get; set; }
+    }
+}
