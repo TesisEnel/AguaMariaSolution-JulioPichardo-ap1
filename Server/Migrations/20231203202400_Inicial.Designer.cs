bd449a2d8b0e35e2c78e01810f3627cbf05a746b
A-EHJ <abrahamelhage2003@gmail.com>
2023-12-03 18:31:26 -0400

    Modificando el Contexto: • Agregue Dbset las clases creadas y modificadas • Elimine parámetros repetidos • Agregando ParametrosEntidadesMuestreoAguas al Model Creating con las relaciones necesarias. • Agregando Empleados y Admins al contexto para tener ya creados. Data • Update a la base de datos. Migrations • Nueva Migracion con todos los cambios

--- Server/Migrations/20231203202400_Inicial.Designer.cs
+++ Server/Migrations/20231203202400_Inicial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AguaMariaSolution.Server.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AguaMariaSolution.Server.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20231203202400_Inicial")]
+    partial class Inicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
